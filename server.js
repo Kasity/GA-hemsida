7ea1058c286ea573f3a125ff98932d0e60203405
Isak <88166181+Kasity@users.noreply.github.com>
2022-08-18 21:50:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 80
 const path = require('path')
-app.use('/', express.static(path.join(__dirname, '/Resources')))
+app.use('/', express.static(path.join(__dirname, '/src')))
 app.use('/', require('./routes/root'))
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'Resources', '404.html'))
+        res.sendFile(path.join(__dirname, 'src', '404.html'))
     } else if (req.accepts('json')) {
         res.json({ "error": "404 Not Found" })
     } else {
